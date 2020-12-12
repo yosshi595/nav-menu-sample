15d6e5d590d1909f62f4a7e13d29f2574bf919f2
ko.shin.727@gmail.com <yoshida@yoshidanoMacBook-Pro.local>
2020-12-12 13:19:16 +0900

    consoleを追加

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const navButton = document.getElementsByClassName('js-nav-button') [0]
 const nav = document.getElementsByClassName('nav')[0]
-
+console.log('test')
 navButton.addEventListener('click', () => {
     nav.classList.toggle('active')
     if (nav.classList.contains('active')) {
